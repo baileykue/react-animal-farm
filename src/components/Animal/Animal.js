d4c84fdff1b7232e9dc1e86d502bb42a1d3f5b2d
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-11-29 14:55:57 -0800

    added animal images to the page

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
-export default function Animal() {
-  return <></>
+export default function Animal(props) {
+  return (
+    <>
+      <div>
+        <img src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
+        <span>`${props.name}`</span>
+      </div>
+    </>
+  )
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './Main.css'
 import background from '../../background.png'
+import Animal from '../Animal/Animal'
+import { animals } from '../../data'
 
 export default function Main() {
   return (
     <main>
       <img src={background} alt="farmyard" />
+      {animals.map((animal) => (
+        <Animal key={animal.name} type={animal.type} name={animals.name} />
+      ))}
     </main>
   )
 }
